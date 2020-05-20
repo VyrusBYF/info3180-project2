0518c62188faa1fc4fa2a35db6a59bfb32655cda
Chafro <ChafroBrown@gmail.com>
2020-05-20 17:17:31 -0500

    finished error handling

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ UPLOAD_FOLDER =  './app/static/uploads'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:info3180@localhost/project2' #"postgresql://user:password@localhost/database" 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qtvjqgwanzhurf:f8176ebe7a770f9500f9824d4a94f276c08b3c3710135beb80a4f470e4a64a00@ec2-34-200-72-77.compute-1.amazonaws.com:5432/db52od3ufs9krn' #"postgresql://user:password@localhost/database" 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 csrf = CSRFProtect(app)
